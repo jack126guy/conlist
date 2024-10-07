a8224542ac4e323f52de1e048c3e5c21604184f2
Jack Grayson <jackgraysonfox@gmail.com>
2024-10-06 21:25:41 -0700

    Add missing import in index

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import ConlistItem from './src/components/ConlistItem.astro';
 import ConlistPast from './src/components/ConlistPast.astro';
 import ConlistFuture from './src/components/ConlistFuture.astro';
+import ConlistUndated from './src/components/ConlistUndated.astro';
 import type { Event, Location } from './src/event';
 
 export {

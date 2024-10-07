5335b46762250dfec953dc37c6938cde3a58bf59
Jack Grayson <jackgraysonfox@gmail.com>
2024-10-06 21:24:29 -0700

    Export ConlistUndated in index file

--- index.ts
+++ index.ts
@@ -3,4 +3,11 @@ import ConlistPast from './src/components/ConlistPast.astro';
 import ConlistFuture from './src/components/ConlistFuture.astro';
 import type { Event, Location } from './src/event';
 
-export { ConlistItem, ConlistPast, ConlistFuture, type Event, type Location };
+export {
+	ConlistItem,
+	ConlistPast,
+	ConlistFuture,
+	ConlistUndated,
+	type Event,
+	type Location,
+};
